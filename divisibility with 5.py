14e52ba9644fa43dfefcc584a43306316b7e2ee3
nyabutolewis33 <109216207+nyabutolewis33@users.noreply.github.com>
2023-01-30 11:48:52 +0300

    Add files via upload

---
+++ divisibility with 5.py
@@ -0,0 +1,6 @@
+#divisibility test
+x=int(input("enter any number x: "))
+if(x%5==0):
+    print("even number")
+else:
+    print("odd number")
---
+++ oddeven.py
@@ -0,0 +1,6 @@
+#odd/even number
+x=int(input("enter any number x: "))
+if(x%2==0):
+    print("even number")
+else:
+    print("odd number")
